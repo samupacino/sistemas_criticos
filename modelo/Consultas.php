920e032496872df10466160f4915be4795727ec6
Samuel Lujan Sarasi <samlsr064@gmail.com>
2022-09-28 00:46:13 -0500

    editando codigo base de datos

--- modelo/Consultas.php
+++ modelo/Consultas.php
@@ -6,21 +6,17 @@
 
         }
         public function insertar($datos){
-            $filasAfectados = false;
             $ds = null;
             $ps = null;
-            echo "asdsa";
             try{
                 $db = Conexion::open();
-                $ps = $db->prepare("INSERT INTO instrumento (instID,instDescripcion,instNormaCodigo) VALUES(?,?,?)");
+                $ps = $db->prepare("INSERT INTO instrumento (codigo,instID,instDescripcion,instNormaCodigo) VALUES(?,?,?)");
                 $ps->execute(array($datos['instrumento'], $datos['descripcion'], $datos['norma']));  
             }catch(PDOException $e){
                 $_SESSION['mensaje'] = "<p id='mensajeServer' >" . $e->getMessage() ;
-                $this->mensaje = $e->getMessage();
             } finally{
                 Conexion::close($ds,$ps);
             } 
-            return $filasAfectados;
         }
         public function listar(){
             $db = null;
@@ -37,18 +33,6 @@
                 Conexion::close($db, $ps);
             }
             return $resultado;
-        }
-        public function pruebaquery(){
-            $datos = ["samuel","lujan","sarasi"];
-            $datosdos = ["ines","lujan","sarasi"];
-
-            $conexion = Conexion::open();
-            $ps = $conexion->prepare("INSERT INTO instrumento (instID,instDescripcion,instNormaCodigo) VALUES(?,?,?)");
-            $ps->execute(array($datos[0],$datos[1],$datos[2]));
-            //$ps->execute(array($datosdos[0],$datosdos[1],$datosdos[2]));
-        }
-        public function getMensaje(){
-            return $this->mensaje;
         }
     }
 ?>
